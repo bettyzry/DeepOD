439ff12b7f6e9d696ab0e89454d46bec0d1bb76b
bettyzry <49117825+bettyzry@users.noreply.github.com>
2023-09-07 10:42:16 +0800

    debug

--- deepod/core/base_model.py
+++ deepod/core/base_model.py
@@ -428,7 +428,7 @@ class BaseDeepAD(metaclass=ABCMeta):
         if self.sample_selection == 0:          # 无操作
             pass
         elif self.sample_selection == 1:        # 保留delta最小的80%
-            save_num = int(self.save_rate * len(self.train_data))
+            save_num = max(int(self.save_rate * len(self.train_data)), int(self.n_samples*0.3))
             delta = train_loss_now - train_loss_past
             index = delta.argsort()[:save_num]
             self.train_data = self.train_data[np.sort(index)]
